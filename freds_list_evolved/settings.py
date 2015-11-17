5fd7db770658acdba23df980d746dba47ed2a63f
Some Guy <abc@abc.com>
2015-11-16 16:49:27 -0800

    added logging to settings

--- freds_list_evolved/settings.py
+++ freds_list_evolved/settings.py
@@ -133,10 +133,36 @@ REST_FRAMEWORK = {
 
 }
 
-# LOGGING = {
-#     'version': 1,
-#     'disable_existing_loggers': False,
-#     'formatter': {
-#         'verbose':
-#     }
-# }
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(modules)s %(messages)s'
+        }
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/general_logs.log'),
+            'formatter': 'verbose'
+        },
+        'mailer': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'peteslist': {
+            'handlers': ['file', 'console', 'mailer'],
+            'level': 'DEBUG',
+            'propagate': True
+        }
+    },
+}
